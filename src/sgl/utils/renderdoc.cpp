d2acfc0f1d96caa3d0ab14493a9e51329b2fd50b
guoxx <guoxx@me.com>
2026-04-28 22:17:15 +0800

    Fix renderdoc android integration, to be verified.

--- src/sgl/utils/renderdoc.cpp
+++ src/sgl/utils/renderdoc.cpp
@@ -75,7 +75,7 @@ public:
 #endif
 #if SGL_HAS_VULKAN
         case DeviceType::vulkan:
-            device_handle = device->native_handles()[0].as<VkInstance>();
+            device_handle = RENDERDOC_DEVICEPOINTER_FROM_VKINSTANCE(device->native_handles()[0].as<VkInstance>());
             break;
 #endif
         default:
@@ -87,7 +87,7 @@ public:
         window_handle = window ? window->window_handle().hwnd : nullptr;
 #elif SGL_LINUX
 #if __ANDROID__
-        window_handle = nullptr;
+        window_handle = window ? window->window_handle().native_window : nullptr;
 #else
         window_handle = window ? (void*)uintptr_t(window->window_handle().xwindow) : nullptr;
 #endif
